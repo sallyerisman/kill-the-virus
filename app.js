79d515f8099e826b342da505a6d79cac6325f7a7
Sally Erisman <sally.erisman@gmail.com>
2020-06-10 09:04:24 +0200

    Set up basic file structure.

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const logger = require('morgan');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/', require('./routes/index'));
+
+// serve static files from public-folder
+app.use(express.static('public'));
+
+module.exports = app;
---
+++ models/index.js
@@ -0,0 +1,4 @@
+/* MODELS */
+
+module.exports = {
+}
